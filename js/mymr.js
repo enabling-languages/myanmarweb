7e690975363ab1842c6dec6a938bd5631902b82d
andjc <lang.support@gmail.com>
2014-08-21 14:13:40 +1000

    Initial set up of myanmarltt/myanmarweb from andjc/jquery-mymr

---
+++ js/mymr.js
@@ -0,0 +1,10 @@
+/**
+* Myanmar Web Typography (mymr.js)
+*
+* Vanilla JS port of jQuery.mymr.js
+* https://github.com/myanmarltt/myanmarweb
+* 
+* Copyright (c) 2014 Myanmar Languages, Typography and Typesetting (MLTT)
+* Released under the MIT License.
+*/
+
